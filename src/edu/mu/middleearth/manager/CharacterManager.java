1c820414f65426a8f2080fb95dccf1acb67d9812
Alexander Lebedev <alexanderlebedev@Alexanders-MacBook-Pro.local>
2025-03-01 16:59:50 -0600

    Added constructor and addCharacter method in CharacterManager

--- src/edu/mu/middleearth/manager/CharacterManager.java
+++ src/edu/mu/middleearth/manager/CharacterManager.java
@@ -4,11 +4,40 @@ import edu.mu.middleearth.characters.MiddleEarthCharacter;
 
 public class CharacterManager {
 	
-	MiddleEarthCharacter[] character;
+	MiddleEarthCharacter[] characters;
 	int size;
+	private static final int INITIAL_CAPACITY = 10;
 	
-
+	/**
+	 * 
+	 */
+	
+	public CharacterManager() {
+		characters = new MiddleEarthCharacter[INITIAL_CAPACITY];
+		size = 0;
+	
+	}
+	/**
+	 * 
+	 * @param c
+	 * @return
+	 */
+	
+	public boolean addCharacter(MiddleEarthCharacter c) {
+		if (c == null) {
+			return false;
+		} else if (size >= characters.length){
+			MiddleEarthCharacter[] newCharacters = new MiddleEarthCharacter[characters.length * 2];
+			for (int i = 0; i < characters.length; i++) {
+				newCharacters[i] = characters[i];
+			}
+			characters = newCharacters;
+		}
+		characters[size++] = c;
+		return true;
 		
 	}
+	
+	
 
 }
