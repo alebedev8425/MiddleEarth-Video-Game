497f6cfacb48d23ff8d682a9384bdb3482194137
kyitz <neb766@gmail.com>
2025-02-28 21:59:52 -0600

    Adding singleton pattern reqiurement

--- src/edu/mu/middleearth/singleton/MiddleEarthCouncil.java
+++ src/edu/mu/middleearth/singleton/MiddleEarthCouncil.java
@@ -1,5 +1,31 @@
 package edu.mu.middleearth.singleton;
 
+import edu.mu.middleearth.manager.CharacterManager;
+
 public class MiddleEarthCouncil {
 
+	
+	// private static variable to hold the single instance
+	private static MiddleEarthCouncil instance = null;
+	
+	// character Manager instance for managing the characters
+	private CharacterManager characterManager;
+	
+	// private constructor to prevent instantiation from outside 
+	private MiddleEarthCouncil() {
+		characterManager = new CharacterManager();
+	}
+	
+	// Public static method to provide access to the single instance
+	public static MiddleEarthCouncil getInstance() {
+		if (instance == null) {
+			instance = new MiddleEarthCouncil();
+		}
+		return instance; 
+	}
+	
+	// method to provide access to character manager
+	public CharacterManager getCharacterManager() {
+		return characterManager;
+	}
 }
