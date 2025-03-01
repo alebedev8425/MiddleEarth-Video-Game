c7c669ab2b10a43fdb36068ac709cf870cc031cc
Alexander Lebedev <alexanderlebedev@Alexanders-MacBook-Pro.local>
2025-03-01 16:35:00 -0600

    Added fields for CharacterManager

--- src/edu/mu/middleearth/manager/CharacterManager.java
+++ src/edu/mu/middleearth/manager/CharacterManager.java
@@ -1,5 +1,14 @@
 package edu.mu.middleearth.manager;
 
+import edu.mu.middleearth.characters.MiddleEarthCharacter;
+
 public class CharacterManager {
+	
+	MiddleEarthCharacter[] character;
+	int size;
+	
+
+		
+	}
 
 }
