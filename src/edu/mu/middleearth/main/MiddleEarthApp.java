f07a38aca64d2301a12aacfc42e178f3b9ae72ae
Alexander Lebedev <alexanderlebedev@mwc-050101.dhcp.missouri.edu>
2025-03-05 14:10:30 -0600

    Added JavaDoc to MiddleEarthApp class

--- src/edu/mu/middleearth/main/MiddleEarthApp.java
+++ src/edu/mu/middleearth/main/MiddleEarthApp.java
@@ -6,8 +6,15 @@ public class MiddleEarthApp {
 
 	public static void main(String[] args) {
 		
+		/**
+		 * create a CharacterManager instance using singleton class MiddleEarthCouncil
+		 */
+		
 		CharacterManager manager = MiddleEarthCouncil.getInstance().getCharacterManager();
 
+		/**
+		 * create a menu object and run the display method
+		 */
 		        
 		Menu menu = new Menu(manager);
 		menu.display();
