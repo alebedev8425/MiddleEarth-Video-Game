2d214c7e6d02619b5cc8d2e4cc8a83b368740645
Alexander Lebedev <alexanderlebedev@Alexanders-MacBook-Pro.local>
2025-03-05 12:37:09 -0600

    Removed test code from MiddleEarthApp and implemented the code to initiate the menu

--- src/edu/mu/middleearth/main/MiddleEarthApp.java
+++ src/edu/mu/middleearth/main/MiddleEarthApp.java
@@ -1,26 +1,17 @@
 package edu.mu.middleearth.main;
-import edu.mu.middleearth.characters.*;
+import edu.mu.middleearth.manager.CharacterManager;
+import edu.mu.middleearth.singleton.MiddleEarthCouncil;
 
 public class MiddleEarthApp {
 
 	public static void main(String[] args) {
-		/*
-		 * test code for middleearthcharacter functions, delete as necessary; 
-		 * 1 of each type of race
-		 */
-		Dwarf mydwarf = new Dwarf("tyrion", 100, 32.2);
-		Wizard mywizard =new Wizard("gandolf",100, 10);
-		Human myhuman = new Human("bob",100,15);
-		Orc myorc = new Orc("orcus",100,25);
-		Elf myelf = new Elf("alfonso",100,5);
 		
-		mydwarf.displayInfo();//test display info 
-		System.out.println("race is: "+mydwarf.getRace()); //test race 
-		
-		
-		myhuman.attack(mywizard); 
-		mywizard.displayInfo();  //info after attack, should be lower than initial health stat
-		
-	}//delete test code whenever necessary!!
+		CharacterManager manager = MiddleEarthCouncil.getInstance().getCharacterManager();
 
+		        
+		Menu menu = new Menu(manager);
+		menu.display();
+		    
+
+}
 }
